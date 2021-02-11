75ddcf11b42fced6d7f42cf0f2ed528d4868c546
Web Developer <ts.analista@gmail.com>
2021-02-11 20:48:22 -0300

    add index.php in the .gitignore

--- index.php
+++ index.php
@@ -14,6 +14,27 @@
 </head>
 <body>
 
+
+<h1>Example heading <span class="badge bg-secondary">New</span></h1>
+<h2>Example heading <span class="badge bg-secondary">New</span></h2>
+<h3>Example heading <span class="badge bg-secondary">New</span></h3>
+<h4>Example heading <span class="badge bg-secondary">New</span></h4>
+<h5>Example heading <span class="badge bg-secondary">New</span></h5>
+<h6>Example heading <span class="badge bg-secondary">New</span></h6>
+
+<button type="button" class="btn btn-primary">
+    Notifications <span class="badge bg-secondary">4</span>
+</button>
+
+<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+
 <script src="node_modules\jquery\dist\jquery.js"></script>
 
 
